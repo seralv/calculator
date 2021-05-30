e389907460dc1f4b2c1383b21895087eb7b76a48
gru-sergio <gru.sergio@protonmail.com>
2021-05-30 17:55:13 -0400

    Update styles sass

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ export default function header() {
     const $div = document.createElement( 'div' );
     $div.innerHTML = `
         <div>
-            <h1>CALCULADORA</h1>
+            <h1>DIGITAL CALCULATOR</h1>
         </div>
     `;
     $div.classList.add( 'header' );
